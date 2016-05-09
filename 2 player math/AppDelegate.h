b433bd8f6ff72c4a45b29969229f13a326539846
moiseevanton <athidif94@gmail.com>
2016-05-09 14:32:20 -0400

    Initial Commit

---
+++ 2 player math/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2 player math
+//
+//  Created by Anton Moiseev on 2016-05-09.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2 player math/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2 player math
+//
+//  Created by Anton Moiseev on 2016-05-09.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
